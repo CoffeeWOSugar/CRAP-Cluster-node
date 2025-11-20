86b6471523f38ecaf20d3421b461d55a1fcab008
villiam <ville@Bettan.localdomain>
2025-11-20 14:09:55 +0100

    Fixed bugs

--- build/build_docker.sh
+++ build/build_docker.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Run from inside the docker/ directory
-if [[ ! -f ../docker_dep/*.deb ]]; then
+debs="$(find ../docker_dep/*.deb | wc -l)"
+if [[  debs -eq "0" ]]; then
   echo "No .deb files found. Copy docker .debs here first."
   exit 1
 fi
@@ -11,7 +12,8 @@ echo "Installing Docker Engine and dependencies from local .deb files..."
 sudo apt-get update || true   # will just refresh local metadata; network will fail but it's ok
 
 # Check if docker is already installed
-if [[(docker --version | wc -l) -eq "1"]]; then
+version="$(docker --version | wc -l)"
+if [[ $version -eq "1" ]]; then
     echo "Docker already installed"
     exit 0
 fi
